c561efa0a088eea76faa4b28a2cf7a941355cafe
VISHNU23becs185 <vishnungowda30@gmail.com>
2026-06-20 18:09:27 +0530

    Create pattern10.cpp

---
+++ patterns/pattern10.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+void invertedtriangle(int n){
+   for(int i =0;i<n;i++){
+    for(int j =0;j<=i;j++){
+    cout<<"*";
+}
+   
+    cout<<endl;
+   }
+   for(int i = n-1;i>=1;i--){
+    for(int j=1;j<=i;j++){
+        cout<<"*";
+    }
+    cout<<endl;
+   }
+
+}
+
+
+
+int main(){
+    int n;
+    cout<<"Enter the value of n: ";
+    cin>>n;
+    invertedtriangle(n);
+    return 0;
+}
