3023c5417bad477db18d241f9b65b4874694d327
VISHNU23becs185 <vishnungowda30@gmail.com>
2026-06-21 21:09:58 +0530

    Update simple.cpp

--- simple.cpp
+++ simple.cpp
@@ -1,24 +1,20 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-void fun(int n){
-    int m =n;
-   for(int i =0;i<n;i++){
-    for(int j =0;j<m;j++){
-    cout<<"*";
-    m--;
-   }
-  
-    cout<<endl;
- 
-   }
-  
-}
 
-
-int main(){
+int main() {
     int n;
-    cout<<"Enter the value of n: ";
-    cin>>n;
-    fun(n);
+    cin >> n;
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < i; j++)
+            cout << " ";
+
+        for (int j = 0; j < n - i; j++)
+            cout << "*";
+
+        cout << endl;
+    }
+
+    
     return 0;
 }
