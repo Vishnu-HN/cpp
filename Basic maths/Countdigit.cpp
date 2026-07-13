d9ecd8a6a2ccc20df0fdbc699b87e6d759faacc5
VISHNU23becs185 <vishnungowda30@gmail.com>
2026-07-13 21:33:20 +0530

    Create Countdigit.cpp

---
+++ Basic maths/Countdigit.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n;
+    cout<<"Enter the number: ";
+    cin>>n;
+    cout<<"Number of digits are: "<<int(log10(n)+1);
+    // int count=0;
+    // while(n>0){
+    //     n= n/10;
+    //     count++;
+    // }
+   // cout<<"Number of digits are: "<<count;
+    return 0;
+}
