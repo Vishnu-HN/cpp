d2edc042408dfd7ba83de10b71c23f19f4fe07d3
VISHNU23becs185 <vishnungowda30@gmail.com>
2026-07-21 21:40:05 +0530

    Create printnum.cpp

---
+++ Recurrsion/basic recurrsion/printnum.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+void fun(int i,int n){
+   if(n<i) return ;
+   cout<<i<<endl;
+   fun(i+1,n); 
+}
+int main(){
+    int n;
+    cout<<"Enter the number ";
+    cin>>n;
+    fun(1,n);
+    return 0;
+}
