1893bdc5c50d4b26dce31addf151b512e5f03452
VISHNU23becs185 <vishnungowda30@gmail.com>
2026-08-04 13:04:00 +0530

    Update pair.cpp

--- STL/pair.cpp
+++ STL/pair.cpp
@@ -6,7 +6,7 @@ int main(){
     pair<int,pair<int,int>>p1 = {1,{2,3}};
     cout<<p1.first<<" " <<p1.second.first<<" "<<p1.second.second <<endl;
    pair<int,int>arr[]={{1,2},{2,3},{5,6}};
-   cout<<arr[1].second;
+   cout<<arr[1].first;
 
 
     return 0;
