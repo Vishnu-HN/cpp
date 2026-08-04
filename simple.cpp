162f500bccdc02dd106b8fbf16f04caec40f59a9
VISHNU23becs185 <vishnungowda30@gmail.com>
2026-08-04 13:10:44 +0530

    Update simple.cpp

--- simple.cpp
+++ simple.cpp
@@ -1,16 +1,14 @@
-#include <iostream>
+#include<bits/stdc++.h>
 using namespace std;
-
-int main() {
-    int arr[5];
-    int n = sizeof(arr)/sizeof(arr[0]);
-    cout<<"Enter the elements for an array ";
-   for(int i=0;i<n;i++){
-     cin>>arr[i];
-   }
-   cout<<endl;
-   for(int i=0;i<n;i++){
-     cout<<arr[i];
-   }
+int main(){
+    int n ;
+    cin>>n;
+    cout<<endl;
+    int i =1;
+    while(i<=n){
+        cout<<i<<endl;
+        i++;
+    }
+    
     return 0;
 }
